efff86a839376ddd07bf28e10ef03ea8e18154c6
Soumya_S <soumya.sikder@perceptivesoftware.com>
2014-06-26 01:51:19 -0400

    removed header tag

--- IndustryRollUpView.js
+++ IndustryRollUpView.js
@@ -2,7 +2,6 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     items:[
-        { html:'<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
         //Adding containers
         {
             xtype: 'container',
